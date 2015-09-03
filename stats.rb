91ce8375a278c3923e1586ba6575e7d4d44fc175
Željko Filipin <zeljko.filipin@gmail.com>
2015-09-03 16:56:02 +0200

    Display stats for browser test jobs
    Stats displayed are job duration median in minutes, job name.

--- stats.rb
+++ stats.rb
@@ -1,22 +1,44 @@
+# http://ruby-doc.org/stdlib-2.2.3/libdoc/json/rdoc/JSON.html
+require 'json'
+
 # http://ruby-doc.org/stdlib-2.2.3/libdoc/open-uri/rdoc/OpenURI.html
 require 'open-uri'
-url = 'https://integration.wikimedia.org/ci/job/browsertests-CentralAuth-en.wikipedia.beta.wmflabs.org-linux-firefox-sauce/api/json?tree=builds[duration]'
-open(url) do |f|
 
-  # http://ruby-doc.org/stdlib-2.2.3/libdoc/json/rdoc/JSON.html
-  require 'json'
-  builds = JSON.parse(f.read)
-
-  durations = builds['builds'].collect do |duration|
-    duration['duration']
+def job_names(jenkins_url)
+  jobs_url = "#{jenkins_url}api/json?tree=jobs[name]"
+  open(jobs_url) do |f|
+    jobs = JSON.parse(f.read)
+    names = jobs['jobs'].collect do |name|
+      name['name']
+    end
+    return names
   end
+end
 
-  p durations
+def job_duration_median(jenkins_url, job_name)
+  job_url = "#{jenkins_url}job/#{job_name}/api/json?tree=builds[duration]"
+  open(job_url) do |f|
+    builds = JSON.parse(f.read)
+    durations = builds['builds'].collect do |duration|
+      duration['duration']
+    end
 
   # https://rubygems.org/gems/descriptive_statistics
   require 'descriptive_statistics'
+  return durations.median/60000 # convert ms to min
+  end
+end
 
-  data = durations
-  p durations.median
+def job_name_and_job_duration_median(jenkins_url, jobs)
+  jobs.collect do |job_name|
+    # http://ruby-doc.org/core-2.2.3/Regexp.html
+    [job_name, job_duration_median(jenkins_url, job_name)] if job_name.match(/^browsertests-.*/)
+  end.compact
+end
 
+jenkins_url = 'https://integration.wikimedia.org/ci/'
+jobs = job_names(jenkins_url)
+job_name_and_job_duration_median(jenkins_url, jobs).each do |job_name, median|
+  # puts "%20s %s" % [median, job_name]
+  puts "%10s %s" % [median.round(1), job_name]
 end
